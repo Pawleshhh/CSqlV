763bc3d3b139faeff3a62d59dfb02ab621359885
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-03-06 14:20:12 +0100

    Add first version of the project

---
+++ CSqlV/SqlQueryFormats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSqlV
+{
+    internal static class SqlQueryFormats
+    {
+
+        public static string InsertRowQuery { get; } = "INSERT INTO {0} VALUES ({1});";
+        //public static string InsertRowQuery { get; } = "INSERT INTO {0}({1}) VALUES ({2});";
+
+        //public static string CreateTable { get; } = "CREATE TABLE {0} ({1});";
+
+    }
+}
